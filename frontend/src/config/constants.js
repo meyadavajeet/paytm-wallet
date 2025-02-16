acac47b299ea98c65f1fc9e03e3b7d51563f940c
AJEET YADAV <meyadavajeet@gmail.com>
2025-02-16 10:27:53 +0530

    added signup api working and setItem to localstorage

---
+++ frontend/src/config/constants.js
@@ -0,0 +1,11 @@
+const constants = {
+  APP_NAME: "My App",
+  APP_VERSION: "1.0.0",
+  APP_DESCRIPTION: "My App Description",
+  APP_URL: "https://myapp.com",
+  APP_PORT: 3000,
+  APP_SECRET: "myappsecret",
+  SERVER_URL: "http://localhost:5500/",
+};
+
+export default constants;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,23 +1,70 @@
+import axios from "axios";
 import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
+import { useNavigate } from "react-router-dom";
+import constants from "../config/constants";
+import { useState } from "react";
 
 export const Signup = () => {
-  
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      // Replace with backend call
+      const response = await axios.post(
+        `${constants.SERVER_URL}api/v1/user/signup`,
+        {
+          firstName,
+          lastName,
+          username: email,
+          password,
+        }
+      );
+
+      // set token in local storage and redirect to dashboard
+      localStorage.setItem("token", response.data.token);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox placeholder="John" label={"First Name"} />
-          <InputBox placeholder="Doe" label={"Last Name"} />
-          <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
-          <InputBox placeholder="123456" label={"Password"} />
+          <InputBox
+            placeholder="John"
+            label={"First Name"}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <InputBox
+            placeholder="Doe"
+            label={"Last Name"}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <InputBox
+            placeholder="harkirat@gmail.com"
+            label={"Email"}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputBox
+            placeholder="123456"
+            label={"Password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button label={"Sign up"} onClick={handleSignup} />
           </div>
           <BottomWarning
             label={"Already have an account?"}

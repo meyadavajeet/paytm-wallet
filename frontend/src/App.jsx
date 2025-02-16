2c388bfb6d58bf37ce59fc4b8f8d754395065da5
AJEET YADAV <meyadavajeet@gmail.com>
2025-02-16 08:53:21 +0530

    after design of the frontend using tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+      <div>APP COMPONENT</div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

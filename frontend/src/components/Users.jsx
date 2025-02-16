00a9a6dcee023383663377318e0b8840729626ba
AJEET YADAV <meyadavajeet@gmail.com>
2025-02-16 12:42:55 +0530

    user list

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./Button";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import constants from "../config/constants";
 
 export const Users = () => {
   // Replace with backend call
-  const [users, setUsers] = useState([
-    {
-      firstName: "Harkirat",
-      lastName: "Singh",
-      _id: 1,
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    try {
+      axios
+        .get(`${constants.SERVER_URL}api/v1/user/search?filter=${filter}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((response) => {
+          setUsers(response.data.user);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }, [filter]);
 
   return (
     <>
@@ -19,20 +33,27 @@ export const Users = () => {
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded border-slate-200"
+          onChange={(e) => setFilter(e.target.value)}
         ></input>
       </div>
       <div>
         {users.map((user) => (
-          <User key={user._id} user={user} />
+          <User user={user} />
         ))}
+        {/* {users.length > 0 ? (
+          users.map((user) => <User user={user} />)
+        ) : (
+          <div className="text-center">No users found</div>
+        )} */}
       </div>
     </>
   );
 };
 
 function User({ user }) {
+  const navigate = useNavigate();
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between" key={user._id}>
       <div className="flex">
         <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
           <div className="flex flex-col justify-center h-full text-xl">
@@ -47,7 +68,13 @@ function User({ user }) {
       </div>
 
       <div className="flex flex-col justify-center h-ful">
-        <Button label={"Send Money"} />
+        <Button
+          label={"Send Money"}
+          onClick={(e) => {
+            e.preventDefault();
+            navigate(`/send-money/${user._id}`);
+          }}
+        />
       </div>
     </div>
   );
